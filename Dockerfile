a942eef26fd50accbd9417a55283bb871ae59bb2
MUTHUKARUPPAN <externe.guruvaiya.muthukaruppan@allianz.fr>
2026-04-17 17:12:57 +0530

    fix: add Masterthought HTML report that opens properly in browser

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ COPY . .
 
 RUN mvn dependency:resolve
 
-CMD ["mvn", "clean", "test"]
+CMD ["mvn", "clean", "verify"]
--- src/test/java/com/automation/runners/TestRunner.java
+++ src/test/java/com/automation/runners/TestRunner.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.automation.stepdefinitions", "com.automation.hooks"},
-        plugin = {"pretty", "html:target/cucumber-report.html"},
+        plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json"},
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
