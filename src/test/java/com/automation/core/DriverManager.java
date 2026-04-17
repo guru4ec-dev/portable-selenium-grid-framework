bc979077112b1734ba49d4312019aa2c494b4f56
MUTHUKARUPPAN <externe.guruvaiya.muthukaruppan@allianz.fr>
2026-04-17 18:19:03 +0530

    fix: increase RemoteWebDriver timeout and node wait time

--- src/test/java/com/automation/core/DriverManager.java
+++ src/test/java/com/automation/core/DriverManager.java
@@ -1,8 +1,11 @@
 package com.automation.core;
 
 import java.net.URI;
+import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.HttpCommandExecutor;
+import org.openqa.selenium.remote.http.ClientConfig;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -21,23 +24,22 @@ public class DriverManager {
                         ? System.getenv("GRID_URL")
                         : System.getProperty("gridUrl", "http://localhost:4444");
 
+                ClientConfig clientConfig = ClientConfig.defaultConfig()
+                        .baseUrl(URI.create(gridUrl).toURL())
+                        .connectionTimeout(Duration.ofSeconds(60))
+                        .readTimeout(Duration.ofSeconds(120));
+
+                HttpCommandExecutor executor = new HttpCommandExecutor(clientConfig);
+
                 if (browser.equalsIgnoreCase("chrome")) {
                     ChromeOptions options = new ChromeOptions();
                     options.setPlatformName("LINUX");
-
-                    driver.set(new RemoteWebDriver(
-                            URI.create(gridUrl).toURL(),
-                            options
-                    ));
+                    driver.set(new RemoteWebDriver(executor, options));
 
                 } else if (browser.equalsIgnoreCase("firefox")) {
                     FirefoxOptions options = new FirefoxOptions();
                     options.setPlatformName("LINUX");
-
-                    driver.set(new RemoteWebDriver(
-                            URI.create(gridUrl).toURL(),
-                            options
-                    ));
+                    driver.set(new RemoteWebDriver(executor, options));
                 }
             }
         } catch (Exception e) {
