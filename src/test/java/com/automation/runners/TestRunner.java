8de30c2920463c1c09d97c3defac20ad9c3fa673
MUTHUKARUPPAN <externe.guruvaiya.muthukaruppan@allianz.fr>
2026-04-17 18:33:11 +0530

    fix: remove parallel DataProvider - scenarios run sequentially per browser

--- src/test/java/com/automation/runners/TestRunner.java
+++ src/test/java/com/automation/runners/TestRunner.java
@@ -2,7 +2,6 @@ package com.automation.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -12,9 +11,4 @@ import org.testng.annotations.DataProvider;
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
 }
